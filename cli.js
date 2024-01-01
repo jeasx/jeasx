98e430c26dc39a9d0a574469fb0c129719082db8
Maik Jablonski <jablonski@neoskop.de>
2024-01-01 02:39:33 +0100

    improved cli

--- cli.js
+++ cli.js
@@ -1,34 +1,56 @@
 #!/usr/bin/env node
-import fs from "node:fs";
-
-export {};
+import fs from "node:fs/promises";
 
 switch (process.argv[2]) {
   case "start":
-    await import("./server.js");
+    await start();
     break;
 
   case "build":
-    process.argv[2] = "routes";
-    process.argv[3] = "js";
-    process.argv[4] = "css";
-    clearDist();
-    await import("./esbuild.config.js");
+    await build();
     break;
 
   case "dev":
-    process.env.NODE_ENV = "development";
-    process.argv[2] = "start";
-    process.argv[3] ??= "node_modules/jeasx/ecosystem.config.cjs";
-    clearDist();
-    await import("pm2/lib/binaries/Runtime4Docker.js");
+    await dev();
+    break;
+
+  case "clear":
+    await clear();
+    break;
+
+  case "help":
+    console.info(`Usage: jeasx [start|build|dev|clear|help]`);
     break;
 
   default:
-    console.info(`❌ Error: Unknown command '${process.argv[2]}'.`);
+    console.info(
+      `❌ Error: Unknown command '${process.argv[2]}'.\nUse 'jeasx help' for options.`
+    );
     process.exit(1);
 }
 
-function clearDist() {
-  fs.rmSync("dist", { recursive: true, force: true });
+async function start() {
+  await import("./server.js");
 }
+
+async function build() {
+  process.argv[2] = "routes";
+  process.argv[3] = "js";
+  process.argv[4] = "css";
+  await clear();
+  await import("./esbuild.config.js");
+}
+
+async function dev() {
+  process.env.NODE_ENV = "development";
+  process.argv[2] = "start";
+  process.argv[3] ??= "node_modules/jeasx/ecosystem.config.cjs";
+  await clear();
+  await import("pm2/lib/binaries/Runtime4Docker.js");
+}
+
+async function clear() {
+  await fs.rm("dist", { recursive: true, force: true });
+}
+
+export {};
