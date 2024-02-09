a3e93a20224edd5c506b302ff6851bf85c17eddf
Maik Jablonski <jablonski@neoskop.de>
2024-02-09 20:45:53 +0100

    Strip all whitespace from process.env.ESBUILD_BROWSER_TARGET

--- esbuild.config.js
+++ esbuild.config.js
@@ -17,7 +17,7 @@ const BROWSER_PUBLIC_ENV = Object.keys(process.env)
   );
 
 const ESBUILD_BROWSER_TARGET = process.env.ESBUILD_BROWSER_TARGET
-  ? process.env.ESBUILD_BROWSER_TARGET.trim().split(/\s*,\s*/)
+  ? process.env.ESBUILD_BROWSER_TARGET.replace(/\s/g, "").split(",")
   : ["chrome109", "edge112", "firefox102", "safari16"];
 
 const args = process.argv.slice(2);
