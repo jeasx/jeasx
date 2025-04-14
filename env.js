dedd9a8dc2d2a9d57b4f02d15ed3975076475691
Maik Jablonski <jablonski@neoskop.de>
2025-04-14 21:10:40 +0200

    Aligned env.js with Bun's env file loading (https://bun.sh/docs/runtime/env#setting-environment-variables)

--- env.js
+++ env.js
@@ -4,37 +4,21 @@ import { existsSync } from "node:fs";
  * Load environment variables from .env* files
  * into process.env in the following order:
  *
- * 1. .env.<NODE_ENV>.local
+ * 1. .env.local
  * 2. .env.<NODE_ENV>
- * 3. .env.local
- * 4. .env
- * 5. .env.defaults
+ * 3. .env
  *
- * If a variable already exists in the environment,
+ * If a variable exists in the environment,
  * it will be not overwritten.
  */
 export default function env() {
-  if (!process.loadEnvFile) {
-    console.warn(
-      "🌻 <process.loadEnvFile> is not available. Please ensure your environment is properly configured."
-    );
-    return;
+  if (process.loadEnvFile) {
+    [
+      ".env.local",
+      ...(process.env.NODE_ENV ? [`.env.${process.env.NODE_ENV}`] : []),
+      ".env",
+    ]
+      .filter(existsSync)
+      .forEach(process.loadEnvFile);
   }
-
-  const files = [".env.defaults", ".env", ".env.local"];
-
-  if (process.env.NODE_ENV) {
-    files.push(
-      `.env.${process.env.NODE_ENV}`,
-      `.env.${process.env.NODE_ENV}.local`
-    );
-  }
-
-  files
-    .toReversed()
-    .filter(existsSync)
-    .forEach((file) => {
-      console.info(`🌻 Loading ${file}`);
-      process.loadEnvFile(file);
-    });
 }
