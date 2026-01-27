f44ee3909bcd50b76a7cb6934298323db80c3991
Maik Jablonski <jablonski@neoskop.de>
2026-01-27 11:08:18 +0100

    removed trailing comma in server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ import serverless from "./serverless.js";
 
 await serverless.listen({
   host: process.env.HOST || "::",
-  port: Number(process.env.PORT || 3000),
+  port: Number(process.env.PORT || 3000)
 });
